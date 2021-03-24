fa2165cecc2e0ea618f8d08f0abe90d54d1507c8
Ardian-Kurniawan <ardiankurniawan7@gmail.com>
2021-03-25 02:17:40 +0700

    Update theme.dart

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -1,13 +1,14 @@
 part of 'shared.dart';
 
-class Theme {
+class MyTheme {
   static ThemeData mainTheme() {
     return ThemeData(
       backgroundColor: Color(0xFF161E29),
       primaryColor: Color(0xFF7041FF),
       accentColor: Color(0xFF7041FF),
       visualDensity: VisualDensity.adaptivePlatformDensity,
-      brightness: Brightness.light,
+      brightness: Brightness.dark,
+      fontFamily: 'Bahnschrift',
       // fontFamily: GoogleFonts.lato().fontFamily,
     );
   }
