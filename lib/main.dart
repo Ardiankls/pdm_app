7b7a8f5d21a085befe3601aa21559c3f0b520392
Ardian-Kurniawan <ardiankurniawan7@gmail.com>
2021-03-29 11:09:23 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
         routes: {
           '/': (context) => Splash(),
           Login.routeName: (context) => Login(),
+          Signup.routeName: (context) => Signup(),
         });
   }
 }
