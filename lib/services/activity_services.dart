455b04e3db23258686325f999072283b290e9ca5
Ardian-Kurniawan <ardiankurniawan7@gmail.com>
2021-04-18 23:21:23 +0700

    Update activity_services.dart

--- lib/services/activity_services.dart
+++ lib/services/activity_services.dart
@@ -1,8 +1,10 @@
 part of 'services.dart';
 
 class ActivityServices {
-  var now = new DateTime.now();
-  var formatter = new DateFormat('yyyy-MM-dd HH:mm:ss');
-  String hasil = formatter.format(now);
-  return hasil;
+  static String dateNow() {
+    var now = new DateTime.now();
+    var formatter = new DateFormat('yyyy-MM-dd HH:mm:ss');
+    String hasil = formatter.format(now);
+    return hasil;
+  }
 }
