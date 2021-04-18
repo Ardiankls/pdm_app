5a3d5c7961087142cb1ccaf8082b6851e8649093
Ardian-Kurniawan <ardiankurniawan7@gmail.com>
2021-04-18 23:04:35 +0700

    Create auth_services.dart

---
+++ lib/services/auth_services.dart
@@ -0,0 +1,8 @@
+part of 'services.dart';
+
+class AuthServices {
+  static FirebaseAuth auth = FirebaseAuth.instance;
+  static CollectionReference userCollection =
+      FirebaseFirestore.instance.collection("users");
+  static DocumentReference userDoc;
+}
