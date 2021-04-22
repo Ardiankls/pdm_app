c0b7219a7660ab2cb6008b6a164746bc78001aa1
Ardian-Kurniawan <ardiankurniawan7@gmail.com>
2021-04-23 05:43:10 +0700

    Update activity_services.dart

--- lib/services/activity_services.dart
+++ lib/services/activity_services.dart
@@ -1,11 +1,31 @@
 part of 'services.dart';
 
 class ActivityServices {
-  
   static String dateNow() {
     var now = new DateTime.now();
     var formatter = new DateFormat('yyyy-MM-dd HH:mm:ss');
     String hasil = formatter.format(now);
     return hasil;
+  }
+
+  static void showToast(String msg, Color mycolor) {
+    Fluttertoast.showToast(
+      msg: msg,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      fontSize: 14,
+    );
+  }
+
+  static Container loadings() {
+    return Container(
+      alignment: Alignment.center,
+      width: double.infinity,
+      height: double.infinity,
+      color: Colors.white70,
+      child: SpinKitFadingCircle(
+        size: 50,
+      ),
+    );
   }
 }
