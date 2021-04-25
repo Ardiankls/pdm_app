664544d446a0c29beca9d319a3f9f54eaf1b8db4
Ardian-Kurniawan <ardiankurniawan7@gmail.com>
2021-04-25 23:17:57 +0700

    Create profile.dart

---
+++ lib/ui/pages/profile.dart
@@ -0,0 +1,14 @@
+part of 'pages.dart';
+
+class Profile extends StatefulWidget {
+  static const String routeName = "/profile";
+  @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
