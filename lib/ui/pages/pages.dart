8d7e8c4f68bb685b0595ad1c4dade7d02a13b134
Ardian-Kurniawan <ardiankurniawan7@gmail.com>
2021-04-26 00:13:31 +0700

    Update pages.dart

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -5,8 +5,10 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:pdm_app/models/models.dart';
 import 'package:pdm_app/services/services.dart';
+import 'package:pdm_app/shared/shared.dart';
 
 part 'splash.dart';
 part 'login.dart';
 part 'signup.dart';
 part 'home.dart';
+part 'profile.dart';
