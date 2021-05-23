11ba433d3343db275d9e4bb3ff7ee4948f098a6a
Ardian-Kurniawan <ardiankurniawan7@gmail.com>
2021-05-24 01:26:12 +0700

    Update profile.dart

--- lib/ui/pages/profile.dart
+++ lib/ui/pages/profile.dart
@@ -8,7 +8,49 @@ class Profile extends StatefulWidget {
 
 class _ProfileState extends State<Profile> {
   @override
+  String msg = "Fail";
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Color(0xFF161E29),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 60),
+          child: Column(children: [
+            Row(
+              children: [
+                Icon(Icons.person_rounded),
+                Container(
+                  child: ElevatedButton.icon(
+                      onPressed: () async {
+                        //melanjutkan ke next stepe
+
+                        // String msg = await AuthServices.signUp(users);
+                        await AuthServices.signout().then((value) {
+                          if (value) {
+                            ActivityServices.showToast(
+                                "Login success", Colors.greenAccent);
+                            Navigator.pushReplacementNamed(
+                                context, Login.routeName);
+                          } else {
+                            ActivityServices.showToast(msg, Colors.redAccent);
+                          }
+                        });
+                      },
+                      icon: Icon(Icons.logout),
+                      label: Text("Logout"),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.red[300],
+                        elevation: 0,
+                      )),
+                )
+              ],
+            ),
+          ]),
+        ),
+      ),
+    );
   }
 }
