74585e09cd38f4e61a4fca2880e2d8969fe86907
Ardian-Kurniawan <ardiankurniawan7@gmail.com>
2021-05-23 23:28:52 +0700

    Update home.dart

--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -10,23 +10,30 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: Color(0xFF161E29),
-      body: Container(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: Color(0xFF161E29),
+        body: Container(
           width: double.infinity,
           height: double.infinity,
           child: Padding(
             padding: const EdgeInsets.only(top: 60),
             child: Container(
-                alignment: Alignment.topRight,
-                child: IconButton(
-                  icon: const Icon(Icons.person_rounded, size: 40),
-                  color: Color(0xFF7041FF),
-                  onPressed: () {
-                    Navigator.pushNamed(context, Profile.routeName);
-                  },
-                )),
-          )),
-    );
+              child: Column(
+                children: [
+                  SizedBox(
+                    child: IconButton(
+                      alignment: Alignment.topRight,
+                      icon: const Icon(Icons.person_rounded, size: 40),
+                      color: Color(0xFF7041FF),
+                      onPressed: () {
+                        Navigator.pushNamed(context, Profile.routeName);
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
